5b8da775091fa6a6d39be0ca75a0ddafc6fc77c8
Dhruv Chawla <dchawla228@gmail.com>
2022-09-09 18:50:06 +0530

    endl - new line after cout

--- MyFirstApp/MyFirstApp.cpp
+++ MyFirstApp/MyFirstApp.cpp
@@ -7,10 +7,15 @@ int main()
     // std is the namespace for cout
     // This will output to the console Hello World!
     std::cout << "Hello World!\n";
+    // endl is a substitute for '\n'
+    std::cout << "My name is Dhruv" << std::endl; 
     
     // This is to remove the junk text at the end of the execution
     system("pause>0");
 
     // Need to put ';' at the end of each line to indicate EOL
-}
 
+    // If you wish to run the file without having to open Visual Studio, you can 
+    // Go to the Debug folder and double click the .exe file
+    // Note: This will only run the last built app
+}
