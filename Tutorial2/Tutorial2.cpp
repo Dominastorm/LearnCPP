adb9bffb5dbabe2dd5f7f8dafafd68cb73155449
Dhruv Chawla <dchawla228@gmail.com>
2022-09-09 19:00:17 +0530

    create variable with value, perform operation, return value

---
+++ Tutorial2/Tutorial2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    // variable of type float that stores the annual salary
+    float annualSalary = 50000.99;
+
+    // variable that stores the monthly salary
+    // stores the value of operation on another variable
+    float monthlySalary = annualSalary / 12;
+
+    // return the output to the console
+    std::cout << "Your monthly salary is " << monthlySalary;
+    
+    system("pause>0");
+}
