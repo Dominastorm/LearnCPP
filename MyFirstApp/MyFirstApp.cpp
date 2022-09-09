2ae6a04e48ce94caee7d22b35914f086175845c9
Dhruv Chawla <dchawla228@gmail.com>
2022-09-09 18:42:51 +0530

    Console output without junk text at the end

---
+++ MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,16 @@
+// This is a header file or a library
+// iostream is used to take input or return output 
+#include <iostream>
+
+int main()
+{
+    // std is the namespace for cout
+    // This will output to the console Hello World!
+    std::cout << "Hello World!\n";
+    
+    // This is to remove the junk text at the end of the execution
+    system("pause>0");
+
+    // Need to put ';' at the end of each line to indicate EOL
+}
+
