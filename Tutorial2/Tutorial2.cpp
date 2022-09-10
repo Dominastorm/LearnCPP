feca5f1b87c6c9c8c50f9df32df87dc516370c44
Dhruv Chawla <dchawla228@gmail.com>
2022-09-10 09:38:10 +0530

    opearations during output; char datatype; naming variables

--- Tutorial2/Tutorial2.cpp
+++ Tutorial2/Tutorial2.cpp
@@ -17,5 +17,19 @@ int main()
     // return the output to the console
     std::cout << "Your monthly salary is " << monthlySalary << std::endl;
 
+    // when you calculate the value directly when giving the output, you 
+    // won't be able to access it later, because you didn't store it
+    std::cout << "In 10 years, you will earn " << annualSalary * 10;
+
+    // when you are using characters (length = 1), you need to put them in single quotes   
+    char character = 'a';
+
+    // Naming Variables
+    // 1. Make sure to name your variables such that their purpose is self-explanatory
+    // 2. The only special character you can use while naming a variable is underscore (_)
+    // 3. You can use digits in the variable names, but a digit can't be the first character
+    // 4. You cannot have whitespaces in your variable names
+    // 5. Use camelCase to name variables
+
     system("pause>0");
 }
