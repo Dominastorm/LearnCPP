001532985f58ccb6cac6390b1438fbc33c7a03de
Dhruv Chawla <dchawla228@gmail.com>
2022-09-10 17:13:10 +0530

    Number of days in month -> Switch Case fallthrough

---
+++ NumberOfDaysProgram/NumberOfDaysProgram.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main()
+{
+    int year, month;
+    std::cout << "Enter the year: ";
+    std::cin >> year;
+    std::cout << "Enter the month: ";
+    std::cin >> month;
+
+    switch (month)
+    {
+    // Februaru
+    // 29 days - leap year
+    // 28 days - otherwise
+    case 2:
+        (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ?
+            std::cout << "29 Days Month" << std::endl:
+            std::cout << "28 Days Month" << std::endl;
+        break;
+    // 30 days - April, June, August, September, November
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        std::cout << "30 Days Month" << std::endl;
+        break;
+    // 31 days - January, March, May, July, August, October, December
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        std::cout << "31 Days Month" << std::endl;
+        break;
+    default:
+        std::cout << "Invalid Input" << std::endl;
+        break;
+    }
+    system("pause > 0");
+}
