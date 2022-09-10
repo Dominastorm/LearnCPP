f080f1e0816c582a7afafae504091b4b15c44a5b
Dhruv Chawla <dchawla228@gmail.com>
2022-09-10 09:28:53 +0530

    Taking input from the user

--- Tutorial2/Tutorial2.cpp
+++ Tutorial2/Tutorial2.cpp
@@ -3,14 +3,19 @@
 int main()
 {
     // variable of type float that stores the annual salary
-    float annualSalary = 50000.99;
+    float annualSalary;
+    // prompt the user for input
+    std::cout << "Please enter your annual salary: ";
+    // take input from the user
+    std::cin >> annualSalary;
+
 
     // variable that stores the monthly salary
     // stores the value of operation on another variable
     float monthlySalary = annualSalary / 12;
 
     // return the output to the console
-    std::cout << "Your monthly salary is " << monthlySalary;
-    
+    std::cout << "Your monthly salary is " << monthlySalary << std::endl;
+
     system("pause>0");
 }
